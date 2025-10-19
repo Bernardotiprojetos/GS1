1c41c2d646b31327ee018b13f3e1e75a6707c6f5
Bernardotiprojetos <bernardotiprojetos@gmail.com>
2025-10-19 01:00:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'icon.png'),
+    title: 'Show do Milhao GS1'
+  });
+
+  mainWindow.loadFile('index.html');
+
+  // Remove menu bar
+  mainWindow.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
